131c17c6499ecfcc4cff52c4b255620b1ef1a865
John Víctor <jovijovi2019@hotmail.com>
2022-08-20 14:54:41 -0300

    Finish MenuBar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -10,6 +10,10 @@ import {
   EmailIcon,
   FavoriteIcon,
   ProfileIcon,
+  Botside,
+  Avatar,
+  ProfileData,
+  ExitIcon,
 } from './styles';
 
 import Button from "../Button";
@@ -49,6 +53,17 @@ const MenuBar: React.FC = () => {
           <span>Tweetar</span>
         </Button>
       </Topside>
+
+      <Botside>
+        <Avatar />
+
+        <ProfileData>
+          <strong>João Víctor Gonçalves</strong>
+          <span>@jovijovi_john</span>
+        </ProfileData>
+
+        <ExitIcon />
+      </Botside>
     </Container>
   );
 }
